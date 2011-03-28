23583313cd142e9ab12d631bc344b83b25625473
Josh Oiknine <joiknine@gmail.com>
2011-03-28 00:56:01 -0400

    added the ability to change data- if a value is passed

--- datadash.jquery.js
+++ datadash.jquery.js
@@ -1,7 +1,12 @@
 (function($){
     $.fn.extend({
-        datadash: function(name) {
-            return $(this).attr('data-'+name);
+        datadash: function(name, value) {
+            if(!value){
+                return $(this).attr('data-'+name);
+            } else{
+                $(this).attr('data-'+name, value);
+                return true;
+            }
         }
     });
 })(jQuery);
